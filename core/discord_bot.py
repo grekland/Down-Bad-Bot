0e4db3e50ebdfecc3e74a2c6efa18f242a3cfa0c
saol <edvin.tengvall@gmail.com>
2023-03-20 01:03:42 +0100

    Update discord_bot.py

--- core/discord_bot.py
+++ core/discord_bot.py
@@ -3,4 +3,17 @@ import os
 
 TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 
-bot = hikari.GatewayBot(token=TOKEN,banner=None, )
+bot = hikari.GatewayBot(token=TOKEN)
+
+@bot.listen()
+
+async def ping(what: hikari.GuildMessageCreateEvent) -> None:
+    if not what.is_human:
+        return
+    
+    me = bot.get_me()
+
+    if me.id in what.message.user_mentions_ids:
+        await what.message.attachments(rf"..\res\images\image_1.png")
+
+bot.run()
