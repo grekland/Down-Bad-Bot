35f5ecb2e212af1819f65bade3739c2884369b53
saol <edvin.tengvall@gmail.com>
2023-03-20 01:22:10 +0100

    v1

--- core/discord_bot.py
+++ core/discord_bot.py
@@ -7,13 +7,15 @@ bot = hikari.GatewayBot(token=TOKEN)
 
 @bot.listen()
 
-async def ping(what: hikari.GuildMessageCreateEvent) -> None:
-    if not what.is_human:
+@bot.listen()
+async def ping(event: hikari.GuildMessageCreateEvent) -> None:
+
+    if not event.is_human:
         return
-    
+
     me = bot.get_me()
 
-    if me.id in what.message.user_mentions_ids:
-        await what.message.attachments(rf"..\res\images\image_1.png")
+    if me.id in event.message.user_mentions_ids:
+        await event.message.respond("https://raw.githubusercontent.com/grekland/Down-Bad-Bot/main/res/images/image_1.png")
 
 bot.run()
